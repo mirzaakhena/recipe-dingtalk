be2d32c5b79f99e85b2dd46b7a9c12d25248865e
Mirza Akhena <mirza.akhena@gmail.com>
2021-02-22 12:32:59 +0700

    update svg icon

--- webview.js
+++ webview.js
@@ -1,7 +1,9 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
+
     // get unread messages
-    const count = 1;
+    // TODO will find out later
+    const count = 0;
 
     // set Franz badge
     Franz.setBadge(count);
